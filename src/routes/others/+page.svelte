576cd895f776bee60e961f342cac7161cba007e6
nown1ne <abhinav14863@gmail.com>
2023-01-17 00:38:16 +0530

    more dark mode fixing

--- src/routes/others/+page.svelte
+++ src/routes/others/+page.svelte
@@ -5,12 +5,12 @@
 <div class="h-screen sm:mt-20 mt-10">
     <grid class="grid sm:grid-cols-3 grid-cols-1 gap-3 content-center justify-center p-4">
         {#each data.summaries as {slug, title, subject}}
-        <div class="w-5/6 shadow-sm shadow-primary-500  p-6 mx-auto bg-white border border-gray-200 rounded-lg  dark:bg-gray-800 dark:border-gray-700">
+        <div class="w-5/6 shadow-sm shadow-primary-500  p-6 mx-auto  border rounded-lg  bg-gray-800 border-gray-700">
             <a href="/prof/{slug}">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight  text-white">{title}</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{subject}</p>
-            <a href="/prof/{slug}" class="inline-flex  items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-primary-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-primary-700 dark:hover:bg-primary-500 dark:focus:ring-blue-800">
+            <p class="mb-3 font-normal  text-gray-400">{subject}</p>
+            <a href="/prof/{slug}" class="inline-flex  items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg  focus:ring-4 focus:outline-none  bg-primary-700 hover:bg-primary-500 focus:ring-blue-800">
                 Read more
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
