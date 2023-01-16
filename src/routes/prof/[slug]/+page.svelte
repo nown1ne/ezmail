172a60bcf231a877ad03066dee1c62707e70f99d
nown1ne <abhinav14863@gmail.com>
2023-01-16 21:29:58 +0530

    Js and working done

--- src/routes/prof/[slug]/+page.svelte
+++ src/routes/prof/[slug]/+page.svelte
@@ -1,12 +1,19 @@
 <script>
     export let data;
-    let mail = 'abhinav14863@gmail.com'
+    let mail = 'abhinav14863@gmail.com';
+    let id = 'nobody@gmail.com' ;
+    let send = () => {
+        let body = document.getElementById("text").value;
+        let email = id;
+        let subject = data.post.title;
+        window.location = "mailto:" + email + "?subject=" + subject + "&body=" + body;
+    }
 </script> 
 
+<input type="enter the mail id" bind:value={id}>
+
+<button class="p-10 mx-auto bg-blue-500" on:click={send}> mail send</button>
+
 <h1 class="sm:text-4xl text-3xl text-white mt-10 mb-5 text-center">{data.post.title}</h1>
-<div class="mx-auto text-white max-w-6xl text-lg sm:text-2xl sm:leading-loose leading-relaxed p-6 h-screen">{@html data.post.content}</div>
+<textarea id="text" cols="100" class="mx-auto max-w-full block text-white  text-lg sm:text-2xl sm:leading-loose leading-relaxed p-6 h-screen bg-gray-900">{data.post.content}</textarea>
 
-<!-- <button class="p-10 mx-auto bg-blue-500">
-
-    <a href="mailto:{mail}?subject={data.post.title}&body={data.post.content}">tell a friend</a>
-</button> -->
--- src/routes/prof/data.js
+++ src/routes/prof/data.js
@@ -1,9 +1,20 @@
 export const posts = [
 	{
 		slug: 'welcome',
-		title: 'Welcome to the Science',
+		title: 'Request for Extension on [Assignment Name]',
+		subject: 'Request for extension on an assignment',
 		content:
-			'<p>We hope your brief detention in the relaxation vault has been a pleasant one.</p><p>Your specimen has been processed and we are now ready to begin the test proper.</p>'
+			'Dear Professor [Name],'+
+
+			'\n\nI am writing to request an extension on the [Assignment Name] that is currently due on [Due Date]. Due to [specific reason for needing extension], I have been unable to complete the assignment on time.'+
+			
+			'\nI understand the importance of this assignment for our course and I assure you that I am fully committed to completing it to the best of my abilities. I would greatly appreciate your consideration in granting me an extension to [specific date].'+
+			
+			'\n\nIf you have any questions or concerns, please do not hesitate to contact me.'+
+			
+			'\nThank you for your understanding and support.'+
+			
+			'\n\nSincerely,\n[Your Name]'
 	},
 
 	{
@@ -11,12 +22,35 @@ export const posts = [
 		title: 'Scheduling an office hour',
 		subject:'Request for office hours ',
 		content:
-			'<div class=""> <div class="text-primary-400 font-bold"><strong>Subject</strong>: Request for office hours this ____ <br></div>Dear Prof. <br>Reang, My name is ______. I am a third-year undergraduate student studying EE. I am writing to you to know about your availability this Friday. I am enrolled in your course ____, and did not quite understand the part where you explained about <input type="text" placeholder="  edit this" class="input"> in the last lecture. I was wondering if it would be possible to come to your office during the office hours and get the doubt clarified. I would be grateful to you if you can explain the concept to me once again. <br>Thank you. I look forward to hearing from you. <br>Sincerely,<br><input type="text" placeholder="  name here" class="input my-2" ><br> <input type="text" placeholder="  roll no and branch" class="input"></div>'
+			'Dear Prof.Reang, \nMy name is ______. I am a third-year undergraduate student studying EE. I am writing to you to know about your availability this Friday. I am enrolled in your course ____, and did not quite understand the part where you explained about something in the last lecture. I was wondering if it would be possible to come to your office during the office hours and get the doubt clarified. \n\nI would be grateful to you if you can explain the concept to me once again.Thank you. I look forward to hearing from you.\nSincerely, \n{roll no and branch}'
 	},
 
 	{
-		slug: 'cake',
-		title: 'This was a triumph',
-		content: "<p>I'm making a note here: HUGE SUCCESS.</p>"
+		slug: 'rgrade',
+		title: 'Request for Excused Absences Due to Medical Reasons',
+		subject:'Letter for medical application',
+		content: 'Dear Professor [Name],'+
+
+		'\nI am writing to inform you that I have been experiencing [specific medical condition] that has prevented me from attending class regularly. I have been under the care of a physician and have been following their treatment plan, but it has resulted in a number of absences from class.'+
+		
+		'\nI understand the importance of attendance in your class and I assure you that I have been keeping up with all of the coursework and have been actively participating in online discussions and group projects. I have also been in contact with my classmates to ensure that I am aware of any missed class materials and assignments.'+
+		
+		'\nI would greatly appreciate your consideration in excusing my absences due to my medical condition. I have attached a letter from my physician to this email for your reference. I would also be happy to meet with you to discuss any concerns or questions you may have.'+
+		
+		'\nThank you for your understanding and support.'+
+		
+		'\nSincerely,\n[Your Name]'
+	},
+	{
+		slug: 'recomm',
+		title: 'Request for a letter of recommendation',
+		subject:'Request for a Letter of Recommendation',
+		content: 'Dear Professor [Name],\n\nI hope this email finds you well. I am writing to request your assistance in obtaining a letter of recommendation. I am applying for [specific program or job] and I would greatly appreciate your support.'+
+		
+		'I had the pleasure of taking your class [class name and semester] and I believe that your assessment of my abilities and performance would be highly valuable to the selection committee.'+
+		
+		'I have attached my resume and a list of my achievements to this email for your reference. If you are willing to write me a letter of recommendation, please let me know what specific information you need from me and the deadline for the letter.'+
+		
+		'\n\nThank you for your time and support.\n\nSincerely,\n[Your Name]'
 	}
 ];
