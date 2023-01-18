380af5d5fc4f3f4efffd2770f4b7dca9f12bb336
nown1ne <abhinav14863@gmail.com>
2023-01-18 18:23:13 +0530

    alpha 1.1 mobile changes

--- src/routes/prof/[slug]/+page.svelte
+++ src/routes/prof/[slug]/+page.svelte
@@ -27,7 +27,7 @@
         <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
             <svg class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
         </span>
-        <span class="relative text-lg w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Send Mail</span>
+        <span class="relative text-sm sm:text-lg w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Send Mail</span>
     </div>
     
 </div>
