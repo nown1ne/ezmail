c52bbeddb283cf0e0e6741c94a95c06bd772f5dc
nown1ne <abhinav14863@gmail.com>
2023-01-18 14:56:04 +0530

    dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {primary: {"50":"#eef2ff","100":"#e0e7ff","200":"#c7d2fe","300":"#a5b4fc","400":"#818cf8","500":"#6366f1","600":"#4f46e5","700":"#4338ca","800":"#3730a3","900":"#312e81"}
