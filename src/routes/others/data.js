cbfd56aab3fecddeb983b0b1259f8183960c1c6c
nown1ne <abhinav14863@gmail.com>
2023-01-21 02:25:20 +0530

    v1.9 (only contributions left)

--- src/routes/others/data.js
+++ src/routes/others/data.js
@@ -1,21 +1,87 @@
 export const posts = [
 	{
-		slug: 'welcome',
-		title: 'Welcome to the Aperture Science computer-aided enrichment center',
+		slug: 'support',
+		title: 'Request for Alumni Support',
+		subject:'reaching out to alumni who have experience in [insert relevant field',
 		content:
-			'<p>We hope your brief detention in the relaxation vault has been a pleasant one.</p><p>Your specimen has been processed and we are now ready to begin the test proper.</p>'
+			'Dear [Name],'+
+
+			"\n\nI hope this email finds you well. My name is [Your Name], a student at [University/College Name]."+ 
+			
+			'\nAs part of our studies, we are currently working on a project that aims to [insert project description]. We are reaching out to alumni who have experience in [insert relevant field/industry] for support and guidance.'+
+			
+			'\nWe would greatly appreciate any insights or advice you could share with us regarding [insert specific topic/area of interest]. We would also be honored if you could spare some time to meet with us, either in person or via video call, to discuss the project further.'+
+			
+			'\nWe understand that your time is valuable, and we are more than happy to work around your schedule. Please let us know if this is something you would be interested in, and we can arrange a meeting time that works best for you.'+
+			
+			'\n\nThank you for your time and consideration. We look forward to hearing from you soon.'+
+			
+			'\n\nBest regards,'+
+			'\n[Your Name]'
 	},
 
 	{
-		slug: 'prof',
-		title: 'Scheduling and office hour',
-		content:
-			'<p> <strong>Subject</strong>: Request for office hours this Friday <br>Dear Prof. <br>Reang, My name is Human Cat. I am a third-year undergraduate student studying EE. I am writing to you to know about your availability this Friday. I am enrolled in your course XYZ69A, and did not quite understand the part where you explained about "how people are mean and terrible" in the last lecture. I was wondering if it would be possible to come to your office during the office hours and get the doubt clarified. I would be grateful to you if you can explain the concept to me once again. <br>Thank you. I look forward to hearing from you. <br>Sincerely,<br>Human Cat<br> 170228, BT-EE </p>'
+		slug: 'advice',
+		title: 'Request for Career Advice',
+		subject:'currently exploring career options in [insert field/industry], and I w',
+		content:'Dear [Name],'+
+
+		'\n\nI hope this email finds you well. My name is [Your Name], a student at [University/College Name], and I am reaching out to you as an alumnus/alumna of the university.'+
+		
+		"\nI am currently exploring career options in [insert field/industry], and I was wondering if you would be willing to share your experience and advice with me. I would greatly appreciate any insights you could provide on what it's like to work in the field, what skills and qualifications are needed, and any other advice you might have for someone interested in pursuing a career in [insert field/industry]."+
+		
+		'\nI would also be honored if you could spare some time to meet with me, either in person or via video call, to discuss your career and experience in more detail. I understand that your time is valuable, and I am more than happy to work around your schedule.'+
+		
+		'\n\nThank you for your time and consideration. I look forward to hearing from you soon.'+
+		
+		'\nBest regards,'+
+		'\n[Your Name]'
 	},
 
+
 	{
-		slug: 'cake',
-		title: 'This was a triumph',
-		content: "<p>I'm making a note here: HUGE SUCCESS.</p>"
-	}
+		slug: 'comp',
+		title: 'Request for Information about a Company/Organization',
+		subject:'researching companies/organizations in [insert field/industry]',
+		content:'I hope this email finds you well. My name is [Your Name], a student at [University/College Name], and I am reaching out to you as an alumnus/alumna of the university.'+
+
+		'\n\nI am currently researching companies/organizations in [insert field/industry], and I came across [Company/Organization Name]. I noticed that you are currently working there, and I was wondering if you would be willing to share some information about the company/organization with me.'+
+		
+		'\nI would greatly appreciate any insights you could provide on the company culture, the work environment, the career development opportunities, and any other information that would be helpful for me to know. I would also be honored if you could spare some time to meet with me, either in person or via video call, to discuss the company/organization in more detail.'+
+		
+		'\n\nThank you for your time and consideration. I look forward to hearing from you soon.'+
+		
+		'\n\nBest regards,'+
+		'\n[Your Name]'
+	},
+	{
+		slug: 'network',
+		title: 'Request for Alumni Networking Opportunities',
+		subject:'',
+		content:'Dear [Name],'+
+
+		'\n\nI hope this email finds you well. My name is [Your Name], a student at [University/College Name].'+
+		
+		'\nI am reaching out to you as an alumnus/alumna of [University/College Name] to request your assistance in expanding my professional network. I am currently seeking opportunities in [insert field/industry], and I would greatly appreciate any introductions or connections you could provide.'+
+		
+		'\nI understand that your time is valuable, and I am more than happy to work around your schedule. I would be honored if you could spare some time to meet with me, either in person or via video call, to discuss potential networking opportunities.'+
+		
+		'\nThank you for your time and consideration. I look forward to hearing from you soon.'+
+		
+		'\n\nBest regards,'+
+		'\n[Your Name]'
+	},
+	{
+		slug: '',
+		title: '',
+		subject:'',
+		content:''
+	},
+	{
+		slug: 'advice',
+		title: '',
+		subject:'',
+		content:''
+	},
+	
 ];
