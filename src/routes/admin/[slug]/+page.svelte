1513a1b82477f8395c5828ec903a2c3b06a04f5e
nown1ne <abhinav14863@gmail.com>
2023-01-21 02:41:12 +0530

    alpha 1.91 (minor fix in textarea)

--- src/routes/admin/[slug]/+page.svelte
+++ src/routes/admin/[slug]/+page.svelte
@@ -31,5 +31,5 @@
 </div>
 
 <h1 class="sm:text-4xl text-3xl text-white mt-10 mb-5 text-center">{data.post.title}</h1>
-<textarea id="text" cols="100" class="overflow-hidden mx-auto max-w-full block text-white  text-lg sm:text-2xl sm:leading-loose leading-relaxed p-6 h-screen bg-gray-900">{data.post.content}</textarea>
+<textarea id="text" cols="100" class="mx-auto max-w-full block text-white  text-lg sm:text-2xl sm:leading-loose leading-relaxed p-6 h-screen bg-gray-900">{data.post.content}</textarea>
 
